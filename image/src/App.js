55067a8a7c0fec18cf7263d41abfce547b4a34ec
sahininayanthara <sahininayanthara95@gmail.com>
2019-12-09 02:41:39 +0530

    react image

---
+++ image/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import pic from "./pic.jpg"
+
+function App() {
+  return (
+    <div>
+      <img src={pic} alt="mypic" />
+    </div>
+  );
+}
+
+export default App;
